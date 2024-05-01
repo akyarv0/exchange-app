355e4854a6ea51d6f5101d683f533f11a80cf3e7
akyarv0 <akyarv0@gmail.com>
2024-05-02 00:24:15 +0300

    all currencies added

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../CSS/currency.css";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import { useState } from "react";
@@ -8,21 +8,30 @@ const API_KEY =
   "fca_live_jxh4XKBp6f1tksuMBwlJ6aWow4pMv6foGz8lhywV&base_currency=USD";
 const Currency = () => {
   const [amount, setAmount] = useState(1);
-  const [toCurrency, setToCurrency] = useState("USD");
-  const [fromCurrency, setFromCurrency] = useState("TRY");
+  const [toCurrency, setToCurrency] = useState("TRY");
+  const [fromCurrency, setFromCurrency] = useState("USD");
   const [result, setResult] = useState(0);
+  const [currencyOptions, setCurrencyOptions] = useState([]);
   const exchange = async () => {
     try {
       const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+      console.log(response.data.data);
       console.log(response.data.data[toCurrency]); // Burada veriyi işleyebilirsiniz
+      
 
-      const result = (response.data.data[toCurrency] * amount).toFixed(2);
+setCurrencyOptions(Object.keys(response.data.data));
+      const result = (response.data.data[toCurrency] * amount).toFixed(3);
       setResult(result);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
     
   };
+
+  useEffect(() => {
+    exchange();
+  }, [fromCurrency, amount, toCurrency]);
+
   
 
   return (
@@ -44,9 +53,9 @@ const Currency = () => {
           value={fromCurrency}
           onChange={(e) => setFromCurrency(e.target.value)}
         >
-          <option>USD</option>
-          <option>EUR</option>
-          <option>TRY</option>
+        {currencyOptions.map((currency) => (
+          <option key={currency}>{currency}</option>
+        ))}
         </select>
         <FaArrowAltCircleRight style={{ fontSize: "40px" }} />
         <select
@@ -56,9 +65,9 @@ const Currency = () => {
           value={toCurrency}
           onChange={(e) => setToCurrency(e.target.value)}
         >
-          <option>TRY</option>
-          <option>USD</option>
-          <option>EUR</option>
+         {currencyOptions.map((currency) => (
+          <option key={currency}>{currency}</option>
+        ))}
         </select>
         <input
           type="number"
