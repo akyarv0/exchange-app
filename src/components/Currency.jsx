04d23b2ff002f2ba36c25321e2415239ab68fe3a
akyarv0 <akyarv0@gmail.com>
2024-05-02 01:47:26 +0300

    convert button

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,38 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import "../CSS/currency.css";
 import { FaArrowAltCircleRight } from "react-icons/fa";
-import { useState } from "react";
 import axios from "axios";
+import { useEffect } from "react";
 const BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
-const API_KEY =
-  "fca_live_jxh4XKBp6f1tksuMBwlJ6aWow4pMv6foGz8lhywV&base_currency=USD";
+const API_KEY = "fca_live_jxh4XKBp6f1tksuMBwlJ6aWow4pMv6foGz8lhywV";
+
 const Currency = () => {
   const [amount, setAmount] = useState(1);
   const [toCurrency, setToCurrency] = useState("TRY");
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [result, setResult] = useState(0);
   const [currencyOptions, setCurrencyOptions] = useState([]);
+
   const exchange = async () => {
     try {
       const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
-      console.log(response.data.data);
-      console.log(response.data.data[toCurrency]); // Burada veriyi işleyebilirsiniz
-      
-
-setCurrencyOptions(Object.keys(response.data.data));
-      const result = (response.data.data[toCurrency] * amount).toFixed(3);
+      setCurrencyOptions(Object.keys(response.data.data));
+      const rate = response.data.data[toCurrency];
+      const result = (rate * amount).toFixed(3);
       setResult(result);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
-    
   };
 
+  // Butona tıklanmadan önce döviz seçeneklerini yüklemek için kullanılır
   useEffect(() => {
-    exchange();
-  }, [fromCurrency, amount, toCurrency]);
-
-  
+    const fetchCurrencies = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}`);
+        setCurrencyOptions(Object.keys(response.data.data));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchCurrencies();
+  }, []);
 
   return (
     <div className="currency-div">
@@ -82,7 +86,7 @@ setCurrencyOptions(Object.keys(response.data.data));
           id=""
           className="result"
           value={result}
-          onChange={(e) => setResult(e.target.value)}
+         
           readOnly
         />
       </div>
