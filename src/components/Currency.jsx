774f686c97ccc9d6daa79cb679e6ab24fa123dc6
akyarv0 <akyarv0@gmail.com>
2024-05-01 15:08:05 +0300

    exchange

---
+++ src/components/Currency.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../CSS/currency.css";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+
+const Currency = () => {
+  return (
+    <div className="currency-div">
+      <div className="title">
+        <h3>CURRENCY CONVERTER</h3>
+      </div>
+      <div className="input-div">
+
+        <input type="number" className="amount" />
+        <select name="" id="" className="from-currency-option">
+          <option value="">USD</option>
+          <option value="">EUR</option>
+          <option value="">TL</option>
+        </select>
+        <FaArrowAltCircleRight style={{ fontSize: "80px" }} />
+        <select name="" id="" className="to-currency-option">
+          <option value="">TL</option>
+          <option value="">USD</option>
+          <option value="">EUR</option>
+        </select>
+        <input type="number" name="" id="" className="result" />
+      </div>
+    </div>
+  );
+};
+
+export default Currency;
